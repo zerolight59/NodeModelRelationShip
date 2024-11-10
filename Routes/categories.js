9cfb43efbc8a544745b05766d51b66ad08de262e
zerolight59 <manusankaru10@gmail.com>
2024-11-10 15:36:46 +0530

    add validation on express using joi

--- Routes/categories.js
+++ Routes/categories.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const joi = require('joi');
 
 const router = express.Router();
-
+const app = express();
+app.use(express.json());
 
 
 const categories = [
@@ -21,6 +22,12 @@ router.get('/', (req, res) => {
 
 
 router.post('/api/categories',(req,res)=>{
+    
+    const {error} =validateData(req.body)
+    if (error) {
+        console.log('Validation error:', error.details[0].message);
+        return res.status(400).send(error.details[0].message)
+    }
     const category ={
         id:categories.length+1,
         name: req.body.name
@@ -58,5 +65,13 @@ router.get('/api/categories/:id',(req,res)=>{
     }
     res.send(category)
 });
+
+function validateData(catagory) {
+    const schema = joi.object({
+        name : joi.string().min(3).required()
+    });
+
+    return schema.validate(catagory)
+}
 
 module.exports = router;
