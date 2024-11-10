73dd7a46bf577c499d285842964165202f849c01
zerolight59 <manusankaru10@gmail.com>
2024-11-10 17:19:43 +0530

    connecting to database

--- Routes/categories.js
+++ Routes/categories.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const joi = require('joi');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 const app = express();
 app.use(express.json());
 
 
-const categories = [
-    {id:1,name:"web"},
-    {id:2,name:"mobile"},
-    {id:3,name:"photography"},
-]
+const categorySchema = new mongoose.Schema({
+    name:{type :String ,required :true , minlength : 3 ,maxlength : 7},
+});
 
-router.get('/api/categories',(req,res)=>{
-    res.send(categories);
+const Catagory = mongoose.model('Catagory',categorySchema);
+
+router.get('/api/categories',async (req,res)=>{
+    let catagory = await Catagory.find();
+    res.send(catagory);
 })
 
 router.get('/', (req, res) => {
@@ -21,50 +23,52 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/api/categories',(req,res)=>{
+router.post('/api/categories',async (req,res)=>{
     
     const {error} =validateData(req.body)
     if (error) {
         console.log('Validation error:', error.details[0].message);
         return res.status(400).send(error.details[0].message)
     }
-    const category ={
-        id:categories.length+1,
-        name: req.body.name
-    };
-    categories.push(category);
+    const category = new Catagory({
+        name : req.body.name
+    });
+    await category.save();
     res.send(category);
 })
 
-router.put('/api/categories/:id',(req,res)=>{
-    const category = categories.find(c=>c.id === parseInt(req.params.id));
-    if (!category) {
-        return res.status(404).send("the given catagory id  was not found")
+router.put('/api/categories/:id',async (req,res)=>{
+
+    const {error} =validateData(req.body)
+    if (error) {
+        console.log('Validation error:', error.details[0].message);
+        return res.status(400).send(error.details[0].message)
     }
 
-    category.name=req.body.name;
-    res.send(category)
-});
-
-
-router.delete('/api/categories/:id',(req,res)=>{
-    const category = categories.find(c=>c.id === parseInt(req.params.id));
-    if (!category) {
-        return res.status(404).send("the given catagory id  was not found")
-    }
-
-    const index = categories.indexOf(category);
-    categories.splice(index,1);
-    res.send(category)
-});
-
-router.get('/api/categories/:id',(req,res)=>{
-    const category = categories.find(c=>c.id === parseInt(req.params.id));
+    const category = await Catagory.findByIdAndUpdate(req.params.id,{name : req.body.id},{new :true})
     if (!category) {
         return res.status(404).send("the given catagory id  was not found")
     }
     res.send(category)
 });
+
+
+router.delete('/api/categories/:id',async (req,res)=>{
+    const category = await Catagory.findByIdAndDelete(req.params.id)
+    if (!category) {
+        return res.status(404).send("the given catagory id  was not found")
+    }
+
+    res.send(category)
+});
+
+// router.get('/api/categories/:id',(req,res)=>{
+//     const category = categories.find(c=>c.id === parseInt(req.params.id));
+//     if (!category) {
+//         return res.status(404).send("the given catagory id  was not found")
+//     }
+//     res.send(category)
+// });
 
 function validateData(catagory) {
     const schema = joi.object({
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const categories = require("./Routes/categories");
+const mongoose = require('mongoose');
 
 const app=express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  
+mongoose.connect("mongodb+srv://manusankaru10:DUFpAf65pkRUTcTh@cluster0.v5sqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=> console.log("connection succesfull"))
+.catch(err=>console.error("coundint connect to mongodb",err))
 
 app.use(express.json());
 app.use(categories);
